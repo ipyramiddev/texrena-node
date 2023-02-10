468317adc54d07c5fefe60c0a73b96a3501da324
ipyramiddev <goldendriver0731@gmail.com>
2023-02-10 13:32:04 +0200

    changed getYesterdayDate and added comment to each function

--- utils/dates.js
+++ utils/dates.js
@@ -1,16 +1,36 @@
 const oneDay = 1000*60*60*24;
 
+/**
+* @desc   Function that returns a current date
+* @param     -
+* @return             current date
+*/
+
 exports.getCurrentDate = () => {
   const currentDate = new Date();
 
   return currentDate;
 }
 
+/**
+* @desc   Function that returns a yesterday date
+* @param     -
+* @return             yesterday date
+*/
+
 exports.getYesterdayDate = () => {
-  const yesterday = new Date(Date.now()-oneDay);
-  
-  return yesterday;
+  const currentDate = this.getCurrentDate()
+  const dayBefore = new Date(currentDate-oneDay);
+
+  return dayBefore;
 }
+
+/**
+* @desc   Function that returns a date in isoString format
+* @param     date     any date
+* @return             date in isoString format
+*/
+
 
 exports.formatDateToIsoString = date => {
   const dateToIsoString = new Date(date).toISOString() 
