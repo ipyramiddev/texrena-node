0d9ff0facadae96ec914ab20eb3ab12a7673aad8
ipyramiddev <goldendriver0731@gmail.com>
2023-02-13 13:54:54 +0200

    created url with createApiUrl and config with createApiOptions

--- controllers/accountsController.js
+++ controllers/accountsController.js
@@ -2,6 +2,25 @@ const { useApi } = require('../utils/index')
 
 exports.getAllAccount = async (req, res) => {
     try {
+        const baseUrl = 'https://dev.mycadenzacloud.com/Thingworx/Things/CDZ.Core.AccountsAPI.Thing/Services';
+        const endpoint = '/getAccounts';
+        const queryParams = req.query;
+
+        const url = useApi.createApiUrl(baseUrl, endpoint, queryParams);
+        
+        const options = {
+            headers: req.headers[appKey],
+        };
+
+        const config = useApi.createApiOptions(options);
+
+        const account = await useApi.callApi(url, config);
+
+        res.status(200).json({
+            status: 'success',
+            account: account
+        });
+
         console.log('get all account');
     } catch (err) {
         res.status(500).json({ message: "Error getting account details" });
@@ -17,18 +36,6 @@ exports.getAllAccount = async (req, res) => {
 
 exports.getAccount = async (req, res) => {
     try {
-        const url = 'https://dev.mycadenzacloud.com/Thingworx/Things/CDZ.Core.AccountsAPI.Thing/Services';
-        const config = {
-            headers: req.headers[appKey],
-            method: 'get',
-        };
-        const account = await useApi.callApi(url, config)
-
-        res.status(200).json({
-            status: 'success',
-            account: account
-        });
-        
         console.log('getaccount');
     } catch (err) {
         res.status(500).json({ message: "Error getting account details" });
